4429cffc7e5b6097f55a8aa40b88dfaddd21b243
余文杰 <wenjie.yu@zhejintech.com>
2020-09-09 21:15:46 +0800

    缓存

--- src/views/cloudbeaver/index.vue
+++ src/views/cloudbeaver/index.vue
@@ -3,4 +3,8 @@
     <iframe src="http://123.56.96.151:8079/#/" frameborder="0" :style='{width:"100%",height:"900px"}'></iframe>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'Cloudbeaver'
+}
+</script>
