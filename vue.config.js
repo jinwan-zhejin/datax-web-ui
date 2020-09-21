10daab645d6b47ae9a7e6e3f8aef7dc74f086db9
junyan.ma <junyan.ma@zhejintech.com>
2020-09-21 19:02:16 +0800

    数据质量: 业务术语、文档管理

--- vue.config.js
+++ vue.config.js
@@ -39,9 +39,9 @@ module.exports = {
       // 代理 /dev-api/api 到 http://localhost:8066/api
       [process.env.VUE_APP_API]: {
         // target: `http://localhost:${apiPort}/api`,
-        // ws: false,
-        // target: 'http://47.103.79.104:9527',
-        target: 'http://192.168.3.83:8080', // 曹海生
+        ws: false,
+        target: 'http://47.103.79.104:9527',
+        // target: 'http://192.168.3.83:8080', // 曹海生
         // target: 'http://192.168.3.72:8080', // 王雷
         changeOrigin: true,
         pathRewrite: {
