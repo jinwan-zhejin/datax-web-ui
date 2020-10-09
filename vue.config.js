4758c4402a140e3c4bcad93658bcbbd9202ee753
余文杰 <wenjie.yu@zhejintech.com>
2020-10-09 13:14:19 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -36,33 +36,46 @@ module.exports = {
       errors: true
     },
     proxy: {
+      [process.env.VUE_APP_API]: {
+        ws: false,
+        // target: `http://localhost:${apiPort}/api`,
+        target: 'http://47.103.79.104:9527',
+        // target: 'http://192.168.3.83:8080', // 曹海生
+        // target: 'http://192.168.3.72:8080', // 王雷
+        // target: 'http://192.168.3.70:9090', // 刘向前
+        // target: 'http://192.168.3.74:7000', // 侯峰
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_API]: '/' + process.env.VUE_APP_API
+        }
+      },
       // 元数据接口 - Atlas
-      // '/atlasApi': {
-      //   ws: false,
-      //   target: 'http://123.56.96.151:8079',
-      //   changeOrigin: true,
-      //   pathRewrite: {
-      //     ['^' + '/atlasApi']: '/api/atlas'
-      //   }
-      // },
-      // // Atlas登录
-      // '/j_spring_security_check': {
-      //   ws: false,
-      //   target: 'http://123.56.96.151:8079',
-      //   changeOrigin: true,
-      //   pathRewrite: {
-      //     ['^' + '/j_spring_security_check']: '/j_spring_security_check'
-      //   }
-      // },
-      // // Atlas登录 session
-      // '/session': {
-      //   ws: false,
-      //   target: 'http://123.56.96.151:8079',
-      //   changeOrigin: true,
-      //   pathRewrite: {
-      //     ['^' + '/session']: '/api/atlas/admin/session'
-      //   }
-      // },
+      '/atlasApi': {
+        ws: false,
+        target: 'http://123.56.96.151:8079',
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + '/atlasApi']: '/api/atlas'
+        }
+      },
+      // Atlas登录
+      '/j_spring_security_check': {
+        ws: false,
+        target: 'http://123.56.96.151:8079',
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + '/j_spring_security_check']: '/j_spring_security_check'
+        }
+      },
+      // Atlas登录 session
+      '/session': {
+        ws: false,
+        target: 'http://123.56.96.151:8079',
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + '/session']: '/api/atlas/admin/session'
+        },
+      },
       // '/login': {
       //   target: 'http://47.103.79.104:8080',
       //   changeOrigin: true,
@@ -77,7 +90,7 @@ module.exports = {
       //     ['^' + '/login']: '/login'
       //   }
       // },
-      // // Datains接口
+      // Datains接口
       // '/welcome': {
       //   target: 'http://47.103.79.104:8080',
       //   changeOrigin: true,
@@ -99,22 +112,11 @@ module.exports = {
       // /api
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       // 代理 /dev-api/api 到 http://localhost:8066/api
-      [process.env.VUE_APP_API]: {
-        ws: false,
-        // target: `http://localhost:${apiPort}/api`,
-        target: 'http://47.103.79.104:9527',
-        // target: 'http://192.168.3.83:8080', // 曹海生
-        // target: 'http://192.168.3.72:8080', // 王雷
-        // target: 'http://192.168.3.70:9090', // 刘向前
-        // target: 'http://192.168.3.74:7000', // 侯峰
-        changeOrigin: true,
-        pathRewrite: {
-          ['^' + process.env.VUE_APP_API]: '/' + process.env.VUE_APP_API
-        }
-      },
+
       // /
       // mock 的代理
       // change xxx-api/login => mock/login
+
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://localhost:${port}/mock`,
         target: 'http://47.103.79.104:9527',
