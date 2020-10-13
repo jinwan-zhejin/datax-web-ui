c194f80b852d8059752a1c58e98e3fefb954bb1b
wanghao <hao.wang@zhejintech.com>
2020-10-13 18:32:26 +0800

    部分样式完善/数据共享接口调通/数据源对话框样式完成

--- src/views/datax/jdbc-datasource/index.vue
+++ src/views/datax/jdbc-datasource/index.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <div class="left">
-        普通数据源
-      </div>
-      <div class="right">
-        <el-input
-          v-model="listQuery.datasourceName"
-          clearable
-          placeholder="数据源名称"
-          style="width: 268px;"
-          class="filter-item"
-          @keyup.enter.native="handleFilter"
-        >
-          <el-button slot="append" v-waves style="margin: 0px;padding: 10px 0px;" class="filter-item" type="primary" @click="fetchData">搜索</el-button>
-        </el-input>
-        <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="handleCreate">
-          添加
-        </el-button>
-        <!-- <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
-          reviewer
-        </el-checkbox> -->
-      </div>
+      <el-card class="box-card">
+        <div class="text item">
+          <div class="left">
+            普通数据源
+          </div>
+          <div class="right">
+            <el-input
+              v-model="listQuery.datasourceName"
+              clearable
+              placeholder="数据源名称"
+              style="width: 268px;"
+              class="filter-item"
+              @keyup.enter.native="handleFilter"
+            >
+              <el-button slot="append" v-waves style="margin: 0px;padding: 10px 0px;" class="filter-item" type="primary" @click="fetchData">搜索</el-button>
+            </el-input>
+            <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="showAdd">
+              添加
+            </el-button>
+            <!-- <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
+              reviewer
+            </el-checkbox> -->
+          </div>
+        </div>
+      </el-card>
     </div>
     <el-table
       v-loading="listLoading"
@@ -75,7 +79,7 @@
       layout="total, prev, pager, next, sizes"
       @pagination="fetchData"
     />
-
+    <!-- 原添加编辑对话框 -->
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible" width="800px">
       <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px">
         <el-form-item label="数据源" prop="datasource">
@@ -165,6 +169,164 @@
         <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
       </span>
     </el-dialog>
+    <!-- UI添加对话框 -->
+    <el-dialog :visible.sync="dialogVisible">
+      <div slot="title">
+        <span>{{ diaTit }}</span>
+      </div>
+      <!-- 第一步选择连接类型 -->
+      <el-tabs v-show="currentStep === 1" v-model="activeName" type="border-card" @tab-click="handleClick">
+        <el-tab-pane label="所有类型" name="first">
+          <ul>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/athena_icon_big@2x.png" alt="">
+                <p>Athena</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/azure_icon_big@2x.png" alt="">
+                <p>Azure Sql server</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/dremio_icon_big@2x.png" alt="">
+                <p>Dremio</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/db2_zos_icon_big@2x.png" alt="">
+                <p>DB2</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/informix_icon_big@2x.png" alt="">
+                <p>Informix</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/mysql_icon_big@2x.png" alt="">
+                <p>Mysql</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/edb_icon_big@2x.png" alt="">
+                <p>EDB</p>
+              </a>
+            </li>
+          </ul>
+        </el-tab-pane>
+        <el-tab-pane label="常用类型" name="second">
+          <ul>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/mysql_icon_big@2x.png" alt="">
+                <p>Mysql</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/edb_icon_big@2x.png" alt="">
+                <p>EDB</p>
+              </a>
+            </li>
+          </ul>
+        </el-tab-pane>
+        <el-tab-pane label="SQL" name="third">
+          <ul>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/mysql_icon_big@2x.png" alt="">
+                <p>Mysql</p>
+              </a>
+            </li>
+          </ul>
+        </el-tab-pane>
+        <el-tab-pane label="分析型" name="fourth">
+          <ul>
+            <li>
+              <a>
+                <img src="@/assets/松鼠png/athena_icon_big@2x.png" alt="">
+                <p>Athena</p>
+              </a>
+            </li>
+          </ul>
+        </el-tab-pane>
+      </el-tabs>
+      <!-- 第二步表单 -->
+      <el-form v-show="currentStep === 2" ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="主机" prop="master">
+          <el-input v-model="ruleForm.master" />
+        </el-form-item>
+        <el-form-item label="数据库架构" prop="sql">
+          <el-input v-model="ruleForm.sql" />
+        </el-form-item>
+        <el-form-item label="认证" prop="authentication">
+          <el-select v-model="ruleForm.authentication" placeholder="请选择">
+            <el-option label="master" value="master" />
+            <el-option label="master1" value="master1" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password" />
+        </el-form-item>
+        <el-form-item label="驱动名称" prop="driver">
+          <el-input v-model="ruleForm.driver" />
+        </el-form-item>
+        <el-form-item>
+          <el-button>编辑驱动名称</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- 连接设置表单 -->
+      <div v-show="currentStep === 3" class="set">
+        <p>Server</p>
+        <el-form :model="ServerForm" label-width="100px" class="bgcForm">
+          <el-form-item label="服务器地址:" prop="master">
+            <el-input v-model="ServerForm.serverUrl" />
+          </el-form-item>
+          <el-form-item label="端口:" prop="sql">
+            <el-input v-model="ServerForm.port" />
+          </el-form-item>
+          <el-form-item label="数据库:" prop="username">
+            <el-input v-model="ServerForm.sql" />
+          </el-form-item>
+        </el-form>
+        <p>认证</p>
+        <el-form :model="userForm" label-width="100px" class="bgcForm">
+          <el-form-item label="用户名:">
+            <el-input v-model="userForm.username" />
+          </el-form-item>
+          <el-form-item label="密码:">
+            <el-input v-model="userForm.password" />
+          </el-form-item>
+        </el-form>
+        <p>Advanced</p>
+        <el-form :model="AdvancedForm" label-width="100px" class="bgcForm">
+          <el-form-item label="服务器时区:">
+            <el-input v-model="AdvancedForm.serverTime" />
+          </el-form-item>
+          <el-form-item label="本地客户端:">
+            <el-input v-model="AdvancedForm.client" />
+          </el-form-item>
+        </el-form>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button v-show="currentStep === 2" style="float: left;">测试连接...</el-button>
+        <el-button :disabled="currentStep === 1" @click="lastStep">上一步</el-button>
+        <el-button type="primary" @click="nextStep">下一步</el-button>
+        <el-button @click="cancel">取 消</el-button>
+        <el-button :disabled="currentStep === 3" @click="dialogVisible = false">完 成</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -243,7 +405,46 @@ export default {
       ],
       jdbc: true,
       hbase: false,
-      mongodb: false
+      mongodb: false,
+      dialogVisible: false,
+      diaTit: '选中新连接类型',
+      currentStep: 1,
+      activeName: 'first',
+      ruleForm: {},
+      AdvancedForm: {},
+      ServerForm: {},
+      userForm: {},
+      rules: {
+        master: [
+          { required: true, message: '请输入主机名称', trigger: 'blur' }
+        ],
+        authentication: [
+          { required: true, message: '请选择认证', trigger: 'blur' }
+        ],
+        sql: [
+          { required: true, message: '请输入数据库/架构名称', trigger: 'blur' }
+        ],
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ],
+        driver: [
+          { required: true, message: '请输入驱动名称', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  watch: {
+    currentStep: function(val) {
+      if (val === 1) {
+        console.log(val, this.diaTit)
+        this.diaTit = '选中新连接类型'
+      } else if (val === 2) {
+        this.diaTit = '通用JDBC连接设置'
+        console.log(val, this.diaTit)
+      }
     }
   },
   created() {
@@ -327,6 +528,31 @@ export default {
         jdbcUrl: '',
         jdbcDriverClass: '',
         comments: ''
+      }
+    },
+    // 显示添加对话框
+    showAdd() {
+      this.dialogVisible = true
+    },
+    // tabs标签页方法
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    cancel() {
+      this.dialogVisible = false
+      this.currentStep = 1
+      this.ruleForm = {}
+    },
+    nextStep() {
+      if (this.currentStep < 3) {
+        this.currentStep++
+      }
+      console.log(this.currentStep)
+    },
+    lastStep() {
+      console.log(this.currentStep)
+      if (this.currentStep > 1) {
+        this.currentStep--
       }
     },
     handleCreate() {
@@ -458,46 +684,100 @@ export default {
 .app-container {
   .filter-container {
     overflow: hidden;
-    line-height: 56px;
+    // line-height: 56px;
     background-color: #ffffff;
     padding: 0px;
-    border-radius: 5px 5px 0px 0px;
-    box-shadow:0 2px 12px 0 rgba(0,0,0,.3);
-    .left {
-      float: left;
-      width: 120px;
-      height: 33px;
-      font-size: 24px;
-      font-family: PingFangHK-Medium, PingFangHK;
-      font-weight: 500;
-      color: #333333;
-      margin-left: 24px;
-    }
-    .right {
-      float: right;
-      margin-right: 20px;
-      .el-input {
-        overflow: hidden;
-        .el-input__inner {
-          float: left;
-          width: 200px;
-          height: 35px;
-          line-height: 35px;
-          padding-right: 15px;
-        }
-        .el-input-group__append {
-          float: left;
-          width: 60px;
-          padding: 0px 15px;
-          text-align: center;
-          color: #fff;
-          background-color: #409EFF;
+    // border-radius: 5px 5px 0px 0px;
+    // box-shadow:0 2px 12px 0 rgba(0,0,0,.3);
+    .el-card {
+      .left {
+        float: left;
+        width: 120px;
+        font-size: 24px;
+        font-family: PingFangHK-Medium, PingFangHK;
+        font-weight: 500;
+        color: #333333;
+        margin-left: 24px;
+      }
+      .right {
+        float: right;
+        margin-right: 20px;
+        .el-input {
+          overflow: hidden;
+          .el-input__inner {
+            float: left;
+            width: 200px;
+            height: 35px;
+            line-height: 35px;
+            padding-right: 15px;
+          }
+          .el-input-group__append {
+            float: left;
+            width: 60px;
+            padding: 0px 15px;
+            text-align: center;
+            color: #fff;
+            background-color: #409EFF;
+          }
         }
       }
     }
   }
   .el-table {
     margin-top: 10px;
+  }
+  .pagination-container {
+    background-color: #f5f6fa;
+  }
+  .el-dialog {
+    .el-dialog__header {
+      border-bottom: 1px solid #F3F3F3;
+      padding-bottom: 20px;
+      span {
+        font-size: 24px;
+        font-family: PingFangHK-Medium, PingFangHK;
+        font-weight: 500;
+        color: #333333;
+      }
+    }
+    .el-tabs {
+      .el-tab-pane {
+        ul {
+          overflow: hidden;
+          li {
+            list-style: none;
+            width: 20%;
+            float: left;
+            a {
+              text-align: center;
+              img {
+                margin-top: 10px;
+                display: block;
+                width: 80%;
+              }
+              p {
+                font-size: 16px;
+                margin-top: 20px;
+              }
+            }
+          }
+        }
+      }
+    }
+    .set {
+      p {
+        font-size: 20px;
+        margin: 20px 0px;
+      }
+      .bgcForm {
+        background-color: #f5f6fa;
+        padding: 20px;
+      }
+    }
+    .el-dialog__footer {
+      border-top: 1px solid #F3F3F3;
+      padding: 20px;
+    }
   }
 }
 </style>
