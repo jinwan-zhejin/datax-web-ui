48eb58dddad0d0bf8fa0c7ab93c084406d270788
chengkai.sheng <chengkai.sheng@zhejintech.com>
2020-11-03 18:53:33 +0800

    添加DDL转换

---
+++ src/views/dataDev/ddlTransform/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div style="margin:30px;min-height:705px;backgroundColor: #fff;border-radius: 0px 0px 8px 8px;">
+</div>
+</template>
+
+<script>
+export default {
+    name: 'DDLTransform',
+    components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
